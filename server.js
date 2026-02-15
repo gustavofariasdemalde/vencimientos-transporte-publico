f4884edba52543465de657de5b270269f63c6468
gustavofariasdemalde <fariasdemaldegustavo@gmail.com>
2026-02-15 19:29:10 -0300

    Fix: ConfiguraciÃ³n para Render - simplificar render.yaml y ajustar servidor

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
